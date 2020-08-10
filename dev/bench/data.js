13d4a90eff2fb232cf7405398c8960c116b48871
github-action-benchmark <github@users.noreply.github.com>
2020-08-10 23:04:37 +0000

    add Benchmark (benchmarkjs) benchmark result for b4022040aa50a8ead6c5b9e2af8723cad777f6d0

---
+++ dev/bench/data.js
@@ -0,0 +1,98 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1597100677399,
+  "repoUrl": "https://github.com/ethereumjs/ethereumjs-vm",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "ethereumjs",
+            "username": "ethereumjs"
+          },
+          "committer": {
+            "name": "ethereumjs",
+            "username": "ethereumjs"
+          },
+          "id": "b4022040aa50a8ead6c5b9e2af8723cad777f6d0",
+          "message": "Use benchmark.js",
+          "timestamp": "2020-08-07T09:02:41Z",
+          "url": "https://github.com/ethereumjs/ethereumjs-vm/pull/830/commits/b4022040aa50a8ead6c5b9e2af8723cad777f6d0"
+        },
+        "date": 1597100676202,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Block 9422905",
+            "value": 4039,
+            "range": "±3.38%",
+            "unit": "ops/sec",
+            "extra": "82 samples"
+          },
+          {
+            "name": "Block 9422906",
+            "value": 4008,
+            "range": "±5.90%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "Block 9422907",
+            "value": 3918,
+            "range": "±8.83%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422908",
+            "value": 3977,
+            "range": "±4.78%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
+          },
+          {
+            "name": "Block 9422909",
+            "value": 4017,
+            "range": "±1.68%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
+          },
+          {
+            "name": "Block 9422910",
+            "value": 3958,
+            "range": "±1.41%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422911",
+            "value": 3104,
+            "range": "±16.21%",
+            "unit": "ops/sec",
+            "extra": "70 samples"
+          },
+          {
+            "name": "Block 9422912",
+            "value": 3770,
+            "range": "±1.49%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
+          },
+          {
+            "name": "Block 9422913",
+            "value": 3808,
+            "range": "±1.39%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "Block 9422914",
+            "value": 3676,
+            "range": "±2.47%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
