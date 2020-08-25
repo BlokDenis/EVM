5c19b4137509e8393c0707a5150dc8947e461eda
github-action-benchmark <github@users.noreply.github.com>
2020-08-25 21:20:58 +0000

    add Benchmark (benchmarkjs) benchmark result for bc30440b97c6226c3677c21a2717ce732e574731

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1597100677399,
+  "lastUpdate": 1598390458722,
   "repoUrl": "https://github.com/ethereumjs/ethereumjs-vm",
   "entries": {
     "Benchmark": [
@@ -90,6 +90,100 @@ window.BENCHMARK_DATA = {
             "range": "±2.47%",
             "unit": "ops/sec",
             "extra": "86 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ryan@ryanio.com",
+            "name": "Ryan Ghods",
+            "username": "ryanio"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bc30440b97c6226c3677c21a2717ce732e574731",
+          "message": "Merge pull request #847 from ethereumjs/fix-storing-benchmarks-result\n\nFix storing benchmarks results",
+          "timestamp": "2020-08-25T14:16:13-07:00",
+          "tree_id": "4c5ac90dbfc4e7ddcd8bdf001348db96dba1131b",
+          "url": "https://github.com/ethereumjs/ethereumjs-vm/commit/bc30440b97c6226c3677c21a2717ce732e574731"
+        },
+        "date": 1598390458085,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Block 9422905",
+            "value": 2012,
+            "range": "±5.41%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          },
+          {
+            "name": "Block 9422906",
+            "value": 2088,
+            "range": "±0.95%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          },
+          {
+            "name": "Block 9422907",
+            "value": 1841,
+            "range": "±6.60%",
+            "unit": "ops/sec",
+            "extra": "81 samples"
+          },
+          {
+            "name": "Block 9422908",
+            "value": 2037,
+            "range": "±1.19%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422909",
+            "value": 1994,
+            "range": "±1.46%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422910",
+            "value": 1950,
+            "range": "±1.21%",
+            "unit": "ops/sec",
+            "extra": "88 samples"
+          },
+          {
+            "name": "Block 9422911",
+            "value": 1918,
+            "range": "±1.34%",
+            "unit": "ops/sec",
+            "extra": "86 samples"
+          },
+          {
+            "name": "Block 9422912",
+            "value": 1900,
+            "range": "±1.27%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422913",
+            "value": 1248,
+            "range": "±16.10%",
+            "unit": "ops/sec",
+            "extra": "58 samples"
+          },
+          {
+            "name": "Block 9422914",
+            "value": 1904,
+            "range": "±1.48%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
           }
         ]
       }
