5cdfbf6d380c0d6f8de5103fcc6ca0f602d5d383
github-action-benchmark <github@users.noreply.github.com>
2021-03-23 17:57:54 +0000

    add Benchmark (benchmarkjs) benchmark result for e4eb3288bc8c09589b2cb51da1400ce02113af61

--- dev/bench/vm/data.js
+++ dev/bench/vm/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1616448163444,
+  "lastUpdate": 1616522274674,
   "repoUrl": "https://github.com/ethereumjs/ethereumjs-monorepo",
   "entries": {
     "Benchmark": [
@@ -1880,6 +1880,100 @@ window.BENCHMARK_DATA = {
             "range": "±1.46%",
             "unit": "ops/sec",
             "extra": "81 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Holger.Drewes@gmail.com",
+            "name": "Holger Drewes",
+            "username": "holgerd77"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e4eb3288bc8c09589b2cb51da1400ce02113af61",
+          "message": "devp2p: implement ETH/65 (#1159)\n\n* devp2p -> ETH: added ETH/65 protocol messages (NEW_POOLED_TRANSACTION_HASHES, GET_POOLED_TRANSACTIONS, POOLED_TRANSACTIONS), added tests\r\n* client: added ETH/65 devp2p protocol version (ETH/65 becomes new default)",
+          "timestamp": "2021-03-23T10:54:18-07:00",
+          "tree_id": "ca9f7d624e5b64a71f1162e1539928be64f4fe76",
+          "url": "https://github.com/ethereumjs/ethereumjs-monorepo/commit/e4eb3288bc8c09589b2cb51da1400ce02113af61"
+        },
+        "date": 1616522273938,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Block 9422905",
+            "value": 2081,
+            "range": "±4.00%",
+            "unit": "ops/sec",
+            "extra": "85 samples"
+          },
+          {
+            "name": "Block 9422906",
+            "value": 1971,
+            "range": "±6.88%",
+            "unit": "ops/sec",
+            "extra": "82 samples"
+          },
+          {
+            "name": "Block 9422907",
+            "value": 2076,
+            "range": "±1.26%",
+            "unit": "ops/sec",
+            "extra": "88 samples"
+          },
+          {
+            "name": "Block 9422908",
+            "value": 1922,
+            "range": "±1.40%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          },
+          {
+            "name": "Block 9422909",
+            "value": 1666,
+            "range": "±13.33%",
+            "unit": "ops/sec",
+            "extra": "72 samples"
+          },
+          {
+            "name": "Block 9422910",
+            "value": 2053,
+            "range": "±1.45%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422911",
+            "value": 2002,
+            "range": "±1.67%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422912",
+            "value": 1983,
+            "range": "±1.68%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "Block 9422913",
+            "value": 1320,
+            "range": "±16.13%",
+            "unit": "ops/sec",
+            "extra": "61 samples"
+          },
+          {
+            "name": "Block 9422914",
+            "value": 1886,
+            "range": "±1.96%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
           }
         ]
       }
