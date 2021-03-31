fd6fb8a89825e42fd6e87ac7af3bc0f69c701295
github-action-benchmark <github@users.noreply.github.com>
2021-03-31 21:45:26 +0000

    add Benchmark (benchmarkjs) benchmark result for e4e5be2a2f9e11bab52a56edb17293837ad52ea2

--- dev/bench/vm/data.js
+++ dev/bench/vm/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617215928268,
+  "lastUpdate": 1617227126445,
   "repoUrl": "https://github.com/ethereumjs/ethereumjs-monorepo",
   "entries": {
     "Benchmark": [
@@ -2726,6 +2726,100 @@ window.BENCHMARK_DATA = {
             "range": "±1.79%",
             "unit": "ops/sec",
             "extra": "76 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "Holger.Drewes@gmail.com",
+            "name": "Holger Drewes",
+            "username": "holgerd77"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e4e5be2a2f9e11bab52a56edb17293837ad52ea2",
+          "message": "Merge pull request #1183 from ethereumjs/vm-fix-access-list-generation-issues\n\nVM: fix access list generation bugs in runTx()",
+          "timestamp": "2021-03-31T23:41:35+02:00",
+          "tree_id": "e7429352e73e0063f4d50ae39ed7d4d3bb729249",
+          "url": "https://github.com/ethereumjs/ethereumjs-monorepo/commit/e4e5be2a2f9e11bab52a56edb17293837ad52ea2"
+        },
+        "date": 1617227125257,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Block 9422905",
+            "value": 1658,
+            "range": "±3.20%",
+            "unit": "ops/sec",
+            "extra": "90 samples"
+          },
+          {
+            "name": "Block 9422906",
+            "value": 1591,
+            "range": "±6.33%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
+          },
+          {
+            "name": "Block 9422907",
+            "value": 1649,
+            "range": "±0.91%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
+          },
+          {
+            "name": "Block 9422908",
+            "value": 1620,
+            "range": "±0.96%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
+          },
+          {
+            "name": "Block 9422909",
+            "value": 1404,
+            "range": "±10.50%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "Block 9422910",
+            "value": 1584,
+            "range": "±0.55%",
+            "unit": "ops/sec",
+            "extra": "94 samples"
+          },
+          {
+            "name": "Block 9422911",
+            "value": 1565,
+            "range": "±1.08%",
+            "unit": "ops/sec",
+            "extra": "82 samples"
+          },
+          {
+            "name": "Block 9422912",
+            "value": 1539,
+            "range": "±0.95%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "Block 9422913",
+            "value": 1519,
+            "range": "±1.27%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "Block 9422914",
+            "value": 1479,
+            "range": "±0.58%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
           }
         ]
       }
